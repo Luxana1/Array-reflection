94dc36b63938fc5a4de3ccae4d9840c77a59dd11
Luxana1 <96484101+Luxana1@users.noreply.github.com>
2022-10-19 09:03:41 +0100

    JavaScript
    I removed unwanted random image arrays function and replaced it with similar one. I also wrote a function that generates and link images with an email.

--- script.js
+++ script.js
@@ -1,45 +1,97 @@
-
-// email validate function
+// buttons variables
+var getBtn = document.getElementById("get-btn");
+var submitBtn = document.getElementById("submit-btn");
+// email validate variables
 var emailError = document.getElementById("email-error");
 var imageError = document.getElementById("image-error");
 var submitError = document.getElementById("result");
+var input = document.querySelector('input');
+var validEmail = false;
+var picsumURL = "https://picsum.photos/v2/list?page="
+var users = {};
+var user = [];
 
-function validateEmail( ) {
+var getData =() => {
+    var randomImage = Math.floor(Math.random() * (10) + 1)
+    axios.get(`${picsumURL}${randomImage}&limit=100`)
+    .then(response => {
+        let pic = response.data[`${randomImage}`]["download_url"];
+        document.getElementById("random-image").innerHTML = `<img id="main" src="${pic}" alt="random image">`;
+    });
+};
+
+window.onload = getData();
+
+var getemail = (e) => {
+    e.preventDefault();
+    var randomImage = Math.floor(Math.random() * (10) + 1)
+    axios.get(`${picsumURL}${randomImage}&limit=100`)
+        .then(response => {
+            let email = document.getElementById("contact-email").value;
+            let pic = response.data[`${randomImage}`]["download_url"];
+            if (validateEmail() === false) {
+                return false;
+            } else {
+                if (email in users) {
+                    users[email].push(pic);
+                } else {
+                    users = { ...users, [email]: [pic] };
+                }
+
+                console.log(users);
+                for (var i = 0; i < users[email].length; i++) {
+                    document.getElementById("submissions").innerHTML = `<h3 id=hide> ${JSON.stringify(email)} </h3>`;
+                    document.getElementById("random-image").innerHTML = `<img id="main" src=${pic}/>`;
+                    for (var j = 0; j < users[email].length; j++) {
+                        
+                        let userCollection = "";
+
+                        for (email in users) {
+                            let emailToAdd = `<h3 id=new> ${JSON.stringify(email)} </h3>`;
+                            let imgsToAdd = "";
+
+                            for (var j = 0; j < users[email].length; j++) {
+                                let imageToAdd = `<img id="card" src=${users[email][j]} />`;
+                                imgsToAdd += imageToAdd;
+                            }
+                            userCollection += `${emailToAdd} ${imgsToAdd}`;
+                    }
+                    document.getElementById("submissions").innerHTML += userCollection;
+                }
+            };
+        }
+    });
+};
+
+function changePic(div) {
+    document.getElementById(div).src = "https://picsum.photos/200/300";
+}
+
+getBtn.addEventListener("click", getData);
+submitBtn.addEventListener("click", getemail);
+
+
+// validate email function
+function validateEmail() {
     var email = document.getElementById("contact-email").value;
 
     if (email.length == 0) {
         emailError.innerHTML = "*Email is required";
-        return false;
+        return validEmail = false;
     }
     if (!email.match(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/)) {
         emailError.innerHTML = "*Email must be valid";
-        return false;
+        return validEmail = false;
     }
     emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
-    return true;
+    return validEmail = true;
 }
 
-function validateSubmit() {
-    if (validateEmail()) {
-        return true;
-    }
-    return false;
-}
-
-// display random image function
-const randomImage = new Array();
-randomImage[0] = "https://picsum.photos/200/300";
-randomImage[1] = "https://picsum.photos/200";
-randomImage[2] = "https://picsum.photos/seed/picsum/200/300";
-randomImage[3] = "https://picsum.photos/200/300?grayscale";
-
-for (var i = 0; i < randomImage.length; i++) {
-    var random = Math.floor(Math.random() * randomImage.length);
-    document.getElementById("random-image").src = randomImage[random];
-}
-
-// function that change image when click on the button 
-function changeRandomImage() {
-    var random = Math.floor(Math.random() * randomImage.length);
-    document.getElementById("random-image").src = randomImage[random];
-}
+// function reqListener () {
+//     console.log(this.responseText);
+//   }
+  
+//   const req = new XMLHttpRequest();
+//   req.addEventListener("load", reqListener);
+//   req.open("GET", "https://picsum.photos/200/300");
+//   req.send();
